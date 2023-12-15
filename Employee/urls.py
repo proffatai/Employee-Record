d1c64771a8b4aa780bc2cfceb5538a954ee7b2a8
proffatai <proffatai@gmail.com>
2023-12-15 22:01:34 +0100

    added readme and media files

--- Employee/urls.py
+++ Employee/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home), #if no path is provided, just the website, then it calls views.home
     path('home/',views.home), # if the /home path is reached, it calls view.home as well
     path('message/',views.message)
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
