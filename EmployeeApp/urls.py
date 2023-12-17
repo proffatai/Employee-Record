2a67f7fc22102d1bf6e85ad2d4960ca9236acb15
proffatai <proffatai@gmail.com>
2023-12-17 16:56:57 +0100

    feature to enable user click the name of the employee

--- EmployeeApp/urls.py
+++ EmployeeApp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('<int:id>/', views.employee_detail), #when any integer is passed as the path to the url, employee_detail function is called with the id as the argument
+path('<int:id>/', views.employee_detail, name="profile"), #when any integer is passed as the path to the url, employee_detail function is called with the id as the argument
 ] #app level url
